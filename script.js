ad23d118c81a87bd236e888a2e9540b102cebf0b
mimbla <70109576+kaguya-ma@users.noreply.github.com>
2021-06-20 20:00:12 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+function exchange() {
+  const key = ""
+  let currencyFrom = document.querySelector("#choose-from").value
+  let currencyTo = document.querySelector("#choose-to").value
+  let amount = document.querySelector("#input").value
+  const url = "https://currency-converter5.p.rapidapi.com/currency/convert"
+
+  if(amount) {
+  fetch(`${url}?format=json&from=${currencyFrom}&to=${currencyTo}&amount=${amount}`, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": key,
+		"x-rapidapi-host": "currency-converter5.p.rapidapi.com"
+	}
+})
+  .then(response => response.json())
+  
+  .then(data => document.querySelector("#result").innerHTML=`${data.rates[currencyTo].rate_for_amount}` + ` ${currencyTo}`);
+
+  }
+
+
+
+}
